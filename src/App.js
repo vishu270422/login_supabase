72757d037be2bf95afc54762d63444b30e53de98
vishu270422 <129824992+vishu270422@users.noreply.github.com>
2024-01-12 19:39:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Auth } from '@supabase/auth-ui-react'
+import {ThemeSupa,} from '@supabase/auth-ui-shared'
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_ANON_KEY
+)
+
+const App = () => (
+  <Auth
+    supabaseClient={supabase}
+    appearance={{ theme: ThemeSupa }}
+  />
+)
+export default App;
