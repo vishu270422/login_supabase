65b6de390f7bd5856373c73736780acf97db0a8a
vishu270422 <129824992+vishu270422@users.noreply.github.com>
2024-01-14 16:26:05 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { Auth } from '@supabase/auth-ui-react'
 import {ThemeSupa,} from '@supabase/auth-ui-shared'
 import { createClient } from '@supabase/supabase-js'
+import { supabase } from './components/supabase'
 
-const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_ANON_KEY
-)
+
 
 const App = () => (
   <Auth
